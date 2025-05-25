b519a3bb2aae4c10f7ada4d31dbc62859b8f0df3
kimquynh <kimquynh270703@gmail.com>
2025-05-25 11:48:18 +0700

    fix loading

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -2,7 +2,7 @@ import { FloatButton } from 'antd';
 
 export default function Loading() {
     return (
-        <div className="w-screen h-screen">
+        <div className="w-screen h-screen flex justify-center items-center">
             <FloatButton />
         </div>
     );
